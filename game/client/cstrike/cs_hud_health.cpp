d09d5e152ecf8442085d2a10d62f45906c5452a0
PowerSiderS.X DARK (KiLiDARK) <nikoladark87@gmail.com>
2025-12-18 22:39:04 +0100

    Update cs_hud_health.cpp

--- game/client/cstrike/cs_hud_health.cpp
+++ game/client/cstrike/cs_hud_health.cpp
@@ -29,6 +29,7 @@ using namespace vgui;
 #include "hudelement.h"
 #include "hud_numericdisplay.h"
 #include "cs_gamerules.h"
+#include "cs_hud_color.h"
 
 #include "convar.h"
 
@@ -37,33 +38,33 @@ using namespace vgui;
 //-----------------------------------------------------------------------------
 class CHudHealth : public CHudElement, public CHudNumericDisplay
 {
-	DECLARE_CLASS_SIMPLE( CHudHealth, CHudNumericDisplay );
+        DECLARE_CLASS_SIMPLE( CHudHealth, CHudNumericDisplay );
 
 public:
-	CHudHealth( const char *pElementName );
-	virtual void Init( void );
-	virtual void VidInit( void );
-	virtual void Reset( void );
-	virtual void OnThink();
+        CHudHealth( const char *pElementName );
+        virtual void Init( void );
+        virtual void VidInit( void );
+        virtual void Reset( void );
+        virtual void OnThink();
 
-	virtual void Paint( void );
-	virtual void ApplySchemeSettings( IScheme *scheme );
+        virtual void Paint( void );
+        virtual void ApplySchemeSettings( IScheme *scheme );
 
 private:
-	// old variables
-	int		m_iHealth;
+        // old variables
+        int             m_iHealth;
 
-	int		m_bitsDamage;
+        int             m_bitsDamage;
 
-	CHudTexture *m_pHealthIcon;
+        CHudTexture *m_pHealthIcon;
 
-	CPanelAnimationVarAliasType( float, icon_xpos, "icon_xpos", "0", "proportional_float" );
-	CPanelAnimationVarAliasType( float, icon_ypos, "icon_ypos", "0", "proportional_float" );
+        CPanelAnimationVarAliasType( float, icon_xpos, "icon_xpos", "0", "proportional_float" );
+        CPanelAnimationVarAliasType( float, icon_ypos, "icon_ypos", "0", "proportional_float" );
 
-//	CPanelAnimationVar( Color, m_LowHealthColor, "LowHealthColor", "255 0 0 255" );
+//      CPanelAnimationVar( Color, m_LowHealthColor, "LowHealthColor", "255 0 0 255" );
 
-	float icon_tall;
-	float icon_wide;
+        float icon_tall;
+        float icon_wide;
 
 };
 
@@ -74,7 +75,7 @@ DECLARE_HUDELEMENT( CHudHealth );
 //-----------------------------------------------------------------------------
 CHudHealth::CHudHealth( const char *pElementName ) : CHudElement( pElementName ), CHudNumericDisplay(NULL, "HudHealth"), m_pHealthIcon( NULL )
 {
-	SetHiddenBits( HIDEHUD_HEALTH | HIDEHUD_PLAYERDEAD );
+        SetHiddenBits( HIDEHUD_HEALTH | HIDEHUD_PLAYERDEAD );
 }
 
 //-----------------------------------------------------------------------------
@@ -82,30 +83,30 @@ CHudHealth::CHudHealth( const char *pElementName ) : CHudElement( pElementName )
 //-----------------------------------------------------------------------------
 void CHudHealth::Init()
 {
-	m_iHealth		= 100;
-	m_bitsDamage	= 0;
-	icon_tall		= 0;
-	icon_wide		= 0;
-	SetIndent(true);
-	SetDisplayValue(m_iHealth);
+        m_iHealth               = 100;
+        m_bitsDamage    = 0;
+        icon_tall               = 0;
+        icon_wide               = 0;
+        SetIndent(true);
+        SetDisplayValue(m_iHealth);
 }
 
 void CHudHealth::ApplySchemeSettings( IScheme *scheme )
 {
-	BaseClass::ApplySchemeSettings( scheme );
+        BaseClass::ApplySchemeSettings( scheme );
 
-	if( !m_pHealthIcon )
-	{
-		m_pHealthIcon = gHUD.GetIcon( "health_icon" );
-	}
+        if( !m_pHealthIcon )
+        {
+                m_pHealthIcon = gHUD.GetIcon( "health_icon" );
+        }
 
-	if( m_pHealthIcon )
-	{
+        if( m_pHealthIcon )
+        {
 
-		icon_tall = GetTall() - YRES(2);
-		float scale = icon_tall / (float)m_pHealthIcon->Height();
-		icon_wide = ( scale ) * (float)m_pHealthIcon->Width();
-	}
+                icon_tall = GetTall() - YRES(2);
+                float scale = icon_tall / (float)m_pHealthIcon->Height();
+                icon_wide = ( scale ) * (float)m_pHealthIcon->Width();
+        }
 }
 
 //-----------------------------------------------------------------------------
@@ -113,7 +114,7 @@ void CHudHealth::ApplySchemeSettings( IScheme *scheme )
 //-----------------------------------------------------------------------------
 void CHudHealth::Reset()
 {
-	g_pClientMode->GetViewportAnimationController()->StartAnimationSequence("HealthRestored");
+        g_pClientMode->GetViewportAnimationController()->StartAnimationSequence("HealthRestored");
 }
 
 //-----------------------------------------------------------------------------
@@ -128,48 +129,52 @@ void CHudHealth::VidInit()
 //-----------------------------------------------------------------------------
 void CHudHealth::OnThink()
 {
-	int realHealth = 0;
-	C_BasePlayer *local = C_BasePlayer::GetLocalPlayer();
-	if ( local )
-	{
-		// Never below zero
-		realHealth = MAX( local->GetHealth(), 0 );
-	}
+        int realHealth = 0;
+        C_BasePlayer *local = C_BasePlayer::GetLocalPlayer();
+        if ( local )
+        {
+                // Never below zero
+                realHealth = MAX( local->GetHealth(), 0 );
+        }
 
-	// Only update the fade if we've changed health
-	if ( realHealth == m_iHealth )
-	{
-		return;
-	}
+        // Only update the fade if we've changed health
+        if ( realHealth == m_iHealth )
+        {
+                return;
+        }
 
-	if( realHealth > m_iHealth)
-	{
-		// round restarted, we have 100 again
-		g_pClientMode->GetViewportAnimationController()->StartAnimationSequence("HealthRestored");
-	}
-	else if ( realHealth <= 25 )
-	{
-		// we are badly injured
-		g_pClientMode->GetViewportAnimationController()->StartAnimationSequence("HealthLow");
-	}
-	else if( realHealth < m_iHealth )
-	{
-		// took a hit
-		g_pClientMode->GetViewportAnimationController()->StartAnimationSequence("HealthTookDamage");
-	}
+        if( realHealth > m_iHealth)
+        {
+                // round restarted, we have 100 again
+                g_pClientMode->GetViewportAnimationController()->StartAnimationSequence("HealthRestored");
+        }
+        else if ( realHealth <= 25 )
+        {
+                // we are badly injured
+                g_pClientMode->GetViewportAnimationController()->StartAnimationSequence("HealthLow");
+        }
+        else if( realHealth < m_iHealth )
+        {
+                // took a hit
+                g_pClientMode->GetViewportAnimationController()->StartAnimationSequence("HealthTookDamage");
+        }
 
-	m_iHealth = realHealth;
+        m_iHealth = realHealth;
 
-	SetDisplayValue(m_iHealth);
+        SetDisplayValue(m_iHealth);
 }
 
 void CHudHealth::Paint( void )
 {
-	if( m_pHealthIcon )
-	{
-		m_pHealthIcon->DrawSelf( icon_xpos, icon_ypos, icon_wide, icon_tall, GetFgColor() );
-	}
+        Color hudColor = GetHudColor();
+        
+        if( m_pHealthIcon )
+        {
+                m_pHealthIcon->DrawSelf( icon_xpos, icon_ypos, icon_wide, icon_tall, hudColor );
+        }
 
-	//draw the health icon
-	BaseClass::Paint();
+        SetFgColor( hudColor );
+        
+        //draw the health icon
+        BaseClass::Paint();
 }
