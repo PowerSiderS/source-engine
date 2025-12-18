4a597093175c8bcd3472e46342a31a97396ddb1d
PowerSiderS.X DARK (KiLiDARK) <nikoladark87@gmail.com>
2025-12-18 22:44:18 +0100

    Update hud_c4.cpp

--- game/client/cstrike/hud_c4.cpp
+++ game/client/cstrike/hud_c4.cpp
@@ -11,28 +11,29 @@
 #include "clientmode_csnormal.h"
 #include "cs_gamerules.h"
 #include "hud_numericdisplay.h"
+#include "cs_hud_color.h"
 
 
 class CHudC4 : public CHudElement, public vgui::Panel
 {
 public:
-	DECLARE_CLASS_SIMPLE( CHudC4, vgui::Panel );
+        DECLARE_CLASS_SIMPLE( CHudC4, vgui::Panel );
 
-	CHudC4( const char *name );
+        CHudC4( const char *name );
 
-	virtual bool ShouldDraw();	
-	virtual void Paint();
-	virtual void Init();
+        virtual bool ShouldDraw();      
+        virtual void Paint();
+        virtual void Init();
 
 
 private:
-	CPanelAnimationVar( Color, m_clrIcon, "IconColor", "IconColor" );
-	CPanelAnimationVar( Color, m_clrFlash, "FlashColor", "FlashColor" );
+        CPanelAnimationVar( Color, m_clrIcon, "IconColor", "IconColor" );
+        CPanelAnimationVar( Color, m_clrFlash, "FlashColor", "FlashColor" );
 
-	CHudTexture *m_pIcon;
+        CHudTexture *m_pIcon;
 
-	float m_flNextFlashTime;
-	bool m_bFlash;
+        float m_flNextFlashTime;
+        bool m_bFlash;
 };
 
 
@@ -40,89 +41,94 @@ DECLARE_HUDELEMENT( CHudC4 );
 
 
 CHudC4::CHudC4( const char *pName ) :
-	vgui::Panel( NULL, "HudC4" ), CHudElement( pName )
+        vgui::Panel( NULL, "HudC4" ), CHudElement( pName )
 {
-	SetParent( g_pClientMode->GetViewport() );
-	m_pIcon = NULL;
+        SetParent( g_pClientMode->GetViewport() );
+        m_pIcon = NULL;
 
-	SetHiddenBits( HIDEHUD_PLAYERDEAD );
-	//=============================================================================
-	// HPE_BEGIN:
-	// [tj] Add this to the render group that disappears when the scoreboard is up
-	//=============================================================================
-	RegisterForRenderGroup( "hide_for_scoreboard" );
-	//=============================================================================
-	// HPE_END
-	//=============================================================================
+        SetHiddenBits( HIDEHUD_PLAYERDEAD );
+        //=============================================================================
+        // HPE_BEGIN:
+        // [tj] Add this to the render group that disappears when the scoreboard is up
+        //=============================================================================
+        RegisterForRenderGroup( "hide_for_scoreboard" );
+        //=============================================================================
+        // HPE_END
+        //=============================================================================
 }
 
 
 void CHudC4::Init()
 {
-	m_flNextFlashTime = 0;
-	m_bFlash = false;
+        m_flNextFlashTime = 0;
+        m_bFlash = false;
 }
 
 
 bool CHudC4::ShouldDraw()
 {
-	C_CSPlayer *pPlayer = C_CSPlayer::GetLocalCSPlayer();
+        C_CSPlayer *pPlayer = C_CSPlayer::GetLocalCSPlayer();
 
-	// if we are spectating another player first person, check this player
-	if ( pPlayer && (pPlayer->GetObserverMode() == OBS_MODE_IN_EYE) )
-	{
-		pPlayer = ToCSPlayer( pPlayer->GetObserverTarget() );
-	}
-	
-	//=============================================================================
-	// HPE_BEGIN:
-	// [tj] Added base class call
-	//=============================================================================
-	return pPlayer && pPlayer->HasC4() && CHudElement::ShouldDraw();
-	//=============================================================================
-	// HPE_END
-	//=============================================================================
+        // if we are spectating another player first person, check this player
+        if ( pPlayer && (pPlayer->GetObserverMode() == OBS_MODE_IN_EYE) )
+        {
+                pPlayer = ToCSPlayer( pPlayer->GetObserverTarget() );
+        }
+        
+        //=============================================================================
+        // HPE_BEGIN:
+        // [tj] Added base class call
+        //=============================================================================
+        return pPlayer && pPlayer->HasC4() && CHudElement::ShouldDraw();
+        //=============================================================================
+        // HPE_END
+        //=============================================================================
 }
 
 
 void CHudC4::Paint()
 {
-	if ( !m_pIcon )
-	{
-		m_pIcon = gHUD.GetIcon( "c4" );
-	}
+        if ( !m_pIcon )
+        {
+                m_pIcon = gHUD.GetIcon( "c4" );
+        }
 
-	C_CSPlayer *pPlayer = C_CSPlayer::GetLocalCSPlayer();
+        C_CSPlayer *pPlayer = C_CSPlayer::GetLocalCSPlayer();
 
-	if ( !pPlayer )
-		return;
+        if ( !pPlayer )
+                return;
 
-	if( pPlayer->m_bInBombZone )
-	{
-		if( m_flNextFlashTime < gpGlobals->curtime )
-		{
-			m_bFlash = !m_bFlash;
-			m_flNextFlashTime = gpGlobals->curtime + 0.15;
-		}
-	}
-	else
-	{
-		m_bFlash = false;
-	}
+        if( pPlayer->m_bInBombZone )
+        {
+                if( m_flNextFlashTime < gpGlobals->curtime )
+                {
+                        m_bFlash = !m_bFlash;
+                        m_flNextFlashTime = gpGlobals->curtime + 0.15;
+                }
+        }
+        else
+        {
+                m_bFlash = false;
+        }
 
-	int x, y, w, h;
-	GetBounds( x, y, w, h );
+        int x, y, w, h;
+        GetBounds( x, y, w, h );
 
-	if ( m_pIcon )
-	{
-		if( m_bFlash )
-		{
-			m_pIcon->DrawSelf( 0, 0, w, h, m_clrFlash );
-		}
-		else
-		{
-			m_pIcon->DrawSelf( 0, 0, w, h, m_clrIcon );
-		}
-	}
+        // Use custom HUD color, preserve flash behavior
+        Color hudColor = GetHudColor();
+        Color flashColor = Color( 255, 255, 255, 255 );
+
+        if ( m_pIcon )
+        {
+                if( m_bFlash )
+                {
+                        // Flash to white when in bomb zone
+                        m_pIcon->DrawSelf( 0, 0, w, h, flashColor );
+                }
+                else
+                {
+                        // Normal state uses HUD color
+                        m_pIcon->DrawSelf( 0, 0, w, h, hudColor );
+                }
+        }
 }
-
