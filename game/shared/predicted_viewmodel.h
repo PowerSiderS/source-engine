63e7d042473ac006ef6ed5960b09f761d76c177b
PowerSiderS.X DARK (KiLiDARK) <nikoladark87@gmail.com>
2025-12-23 19:38:38 +0100

    Update predicted_viewmodel.h

--- game/shared/predicted_viewmodel.h
+++ game/shared/predicted_viewmodel.h
@@ -16,41 +16,57 @@
 #include "baseplayer_shared.h"
 #include "shared_classnames.h"
 
+#ifdef CSTRIKE_DLL
+#include "weapon_csbase.h" 
+        #ifdef CLIENT_DLL
+                #include "c_cs_player.h" 
+        #endif
+#endif
+
 #if defined( CLIENT_DLL )
 #define CPredictedViewModel C_PredictedViewModel
 #endif
 
 class CPredictedViewModel : public CBaseViewModel
 {
-	DECLARE_CLASS( CPredictedViewModel, CBaseViewModel );
+        DECLARE_CLASS( CPredictedViewModel, CBaseViewModel );
 public:
 
-	DECLARE_NETWORKCLASS();
+        DECLARE_NETWORKCLASS();
 
-	CPredictedViewModel( void );
-	virtual ~CPredictedViewModel( void );
-							
-	virtual void CalcViewModelLag( Vector& origin, QAngle& angles, QAngle& original_angles );
+        CPredictedViewModel( void );
+        virtual ~CPredictedViewModel( void );
+                                                        
+        virtual void CalcViewModelLag( Vector& origin, QAngle& angles, QAngle& original_angles );
+        virtual void AddViewModelBob( CBasePlayer *owner, Vector& eyePosition, QAngle& eyeAngles );
+
+#if defined( CLIENT_DLL ) 
+        BobState_t              &GetBobState() { return m_BobState; } 
+#endif
 
 #if defined( CLIENT_DLL )
-	virtual bool ShouldPredict( void )
-	{
-		if ( GetOwner() && GetOwner() == C_BasePlayer::GetLocalPlayer() )
-			return true;
+        virtual bool ShouldPredict( void )
+        {
+                if ( GetOwner() && GetOwner() == C_BasePlayer::GetLocalPlayer() )
+                        return true;
 
-		return BaseClass::ShouldPredict();
-	}
+                return BaseClass::ShouldPredict();
+        }
 #endif
 
 private:
-	
+        
 #if defined( CLIENT_DLL )
 
-	// This is used to lag the angles.
-	CInterpolatedVar<QAngle> m_LagAnglesHistory;
-	QAngle m_vLagAngles;
+        // This is used to lag the angles.
+        CInterpolatedVar<QAngle> m_LagAnglesHistory;
+        QAngle m_vLagAngles;
 
-	CPredictedViewModel( const CPredictedViewModel & ); // not defined, not accessible
+        CPredictedViewModel( const CPredictedViewModel & ); // not defined, not accessible
+        #ifdef CSTRIKE_DLL 
+protected: 
+        BobState_t                      m_BobState;                     // view model head bob state
+        #endif
 
 #endif
 };
