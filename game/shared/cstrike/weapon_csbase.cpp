b4d99d9cf95c1869b77acddddf50af12696f33d8
PowerSiderS <nikoladark87@gmail.com>
2026-09-17 12:57:05 +0100

    reduce viewmodel wobble
    cherrybtw commit

--- game/shared/cstrike/weapon_csbase.cpp
+++ game/shared/cstrike/weapon_csbase.cpp
@@ -1903,7 +1903,7 @@ bool CWeaponCSBase::IsUseable()
 		}
 
 		g_verticalBob = speed*0.005f;
-		g_verticalBob = g_verticalBob*0.3 + g_verticalBob*0.7*sin(cycle);
+		g_verticalBob = g_verticalBob*0.15 + g_verticalBob*0.35*sin(cycle);
 
 		g_verticalBob = clamp( g_verticalBob, -7.0f, 4.0f );
 
@@ -1921,7 +1921,7 @@ bool CWeaponCSBase::IsUseable()
 		}
 
 		g_lateralBob = speed*0.005f;
-		g_lateralBob = g_lateralBob*0.3 + g_lateralBob*0.7*sin(cycle);
+		g_lateralBob = g_lateralBob*0.15 + g_lateralBob*0.35*sin(cycle);
 		g_lateralBob = clamp( g_lateralBob, -7.0f, 4.0f );
 
 		//NOTENOTE: We don't use this return value in our case (need to restructure the calculation function setup!)
